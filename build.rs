c595de14be05af352725f454eb86801516847640
ysc <skyholmes@163.com>
2023-06-21 17:13:27 +0800

    route guide

--- build.rs
+++ build.rs
@@ -2,5 +2,6 @@ use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     tonic_build::compile_protos("proto/helloworld/helloworld.proto")?;
+    tonic_build::compile_protos("proto/routeguide/route_guide.proto")?;
     Ok(())
 }
---
+++ src/routeguide/data.rs
@@ -0,0 +1,32 @@
+use std::{fs::File, path::PathBuf};
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Feature {
+    location: Location,
+    name: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Location {
+    latitude: i32,
+    longitude: i32,
+}
+
+pub fn load() -> Vec<crate::routeguide::Feature> {
+    let data_dir = PathBuf::from_iter([std::env!("CARGO_MANIFEST_DIR"), "data"]);
+    let file = File::open(data_dir.join("route_guide_db.json")).expect("failed to open data file");
+    let decoded: Vec<Feature> =
+        serde_json::from_reader(&file).expect("failed to deserialize features");
+    decoded
+        .into_iter()
+        .map(|feature| crate::routeguide::Feature {
+            name: feature.name,
+            location: Some(crate::routeguide::Point {
+                longitude: feature.location.longitude,
+                latitude: feature.location.latitude,
+            }),
+        })
+        .collect()
+}
